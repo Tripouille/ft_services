22bc0ae346446594743a33212b75c5fb27fba514
jgambard <jgambard@student.42lyon.fr>
2020-07-03 19:56:10 +0200

    nginx port 80 ok

--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
+#!/bin/sh
 GREEN='\033[0;32m'
 NC='\033[0m'
-#echo "Before sudo"
+#Reset
+docker rm -f $(docker ps -aq)
+#docker rmi $(docker images -aq)
+minikube delete
+rm -rf ~/.minikube
+#Setup
 sudo usermod -aG docker $USER
-#echo "After sudo"
-#(newgrp docker)
-echo "${GREEN}Starting minikube...${NC}"
+echo "${GREEN}Starting minikube ...${NC}"
 minikube start --vm-driver=docker
-echo "${GREEN}Applying MetalLB...${NC}"
+
+echo "${GREEN}Applying MetalLB ...${NC}"
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+echo "${GREEN}Configuring MetalLB ...${NC}"
 IP=$(minikube ip | cut -d '.' -f 1-3)
-echo "${GREEN}Configuring MetalLB...${NC}"
 sed "s/IPADDRESSES/"$IP.1-$IP.254"/" srcs/metallb/configmap_base.yaml > srcs/metallb/configmap.yaml
 kubectl create -f srcs/metallb/configmap.yaml
-echo "${GREEN}Creating nginx server...${NC}"
-echo "${GREEN}Building nginx image...${NC}"
-#PORT=$(minikube service --url nginx | cut -d ':' -f 3)
-#sed s/PORT/$PORT/ srcs/nginx/default_base > srcs/nginx/default
-docker build -t mynginx -f srcs/nginx/Dockerfile srcs/nginx/
 
+echo "${GREEN}Deploying service ...${NC}"
+eval $(minikube docker-env)
+
+echo "${GREEN}Creating nginx server ...${NC}"
+echo "${GREEN}Building nginx image ...${NC}"
+docker build -t mynginx -f srcs/nginx/Dockerfile srcs/nginx/
+echo "${GREEN}Applying nginx yaml ...${NC}"
 kubectl apply -f srcs/nginx/nginx.yaml
-kubectl expose deploy nginx --port 80 --type LoadBalancer
-#kubectl create deploy nginx --image=mynginx
 echo "${GREEN}Finished !${NC}"
